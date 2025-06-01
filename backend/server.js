84d64d54935ec98a0b6979f652bf5ea3e9afb1fe
gabrielcarvalh00 <gcsantos.cic@uesc.br>
2025-06-01 20:00:24 -0300

    new update

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const cors = require('cors'); // Para permitir requisições de diferentes origens, por exemplo, do Front
 
+
 // 2. Inicialização do aplicativo Express
 const app = express();
 
@@ -11,31 +12,33 @@ app.use(express.json());
 
 //rederizando a pagina html
 app.use(express.static('./')); 
+app.use('/images', express.static('images'));
+
 
 // 4. Armazenamento de Dados (em memória por enquanto)
 let exercicios = [
   {
     id: 1,
-    titulo: "Capitais do Brasil",
-    descricao: "Qual é a capital do Brasil?",
-    mapaSelecionado: "Mapa do Brasil",
+    titulo: "Geomatria plana",
+    descricao: "Qual a area do retangulo",
+    mapaSelecionado: "Geometria",
     nivelDificuldade: "Fácil",
-    alternativas: ["São Paulo", "Brasília", "Rio de Janeiro", "Salvador"]
+    alternativas: ["190", "220", "450", "1000"]
   },
   {
-    id: 2,
+    id: 2, //dont show
     titulo: "Matemática Básica",
     descricao: "Quanto é 9 + 6?",
-    mapaSelecionado: "Nenhum",
+    mapaSelecionado: "Nenhum", //show the same image for every exercice;
     nivelDificuldade: "Fácil",
-    alternativas: ["14", "15", "16", "17"]
+    alternativas: ["14", "15", "16", "17"] //dont show
   }
 ];
 
 
 let proximoId = 1; // id para identificar cada elemento
 
-// 5. Definição das Rotas
+// Adicionando um novo exercicio
 app.post('/exercicios', (req, res) => {
   const { titulo, descricao, mapaSelecionado, alternativas, nivelDificuldade } = req.body;
 
@@ -58,16 +61,19 @@ app.post('/exercicios', (req, res) => {
 
 });
 
-//apenas exibir os exercicios na tela
+//Apenas exibir os exerciciositais na tela
 app.get('/exercicios', (req, res) => {
+  //colocando conteudo principal de exercicio em um novo array
     const exerciciosResumidos = exercicios.map(({ id, titulo, nivelDificuldade }) => ({
         id,
         titulo,
         nivelDificuldade // Para possiveis filtros ou ordenações futuras
     }));
+    //enviando o status 200 afirmando que esta ok, e junto o novo arquivoi json filtrado
     res.status(200).json(exerciciosResumidos);
 });
 
+//find exercicio for id;
 app.get('/exercicios/:id', (req, res) => {
   const idParaBuscar = parseInt(req.params.id);
   const exercicio = exercicios.find(e => e.id === idParaBuscar);
@@ -79,6 +85,7 @@ app.get('/exercicios/:id', (req, res) => {
   }
 });
 
+//Edit exercice
 app.put('/exercicios/:id', (req, res) => {
   const idParaBuscar = parseInt(req.params.id);
   const { titulo, descricao, mapaSelecionado, nivelDificuldade, alternativas } = req.body;
@@ -103,6 +110,7 @@ app.put('/exercicios/:id', (req, res) => {
 
 });
 
+//Delet exercice
 app.delete('/exercicios/:id', (req, res) => {
   const idParaDeletar = parseInt(req.params.id);
   const tamanhoOriginal = exercicios.length;
